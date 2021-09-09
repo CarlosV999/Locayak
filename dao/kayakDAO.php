4b6b11ba32f5064db9e69cb6bd243b1d71bf3c82
TMRomain <romainover51@gmail.com>
2021-09-09 21:24:40 +0000

    Ajout de l'ajout de Kayak dans le DAO Kayak #15

--- dao/kayakDAO.php
+++ dao/kayakDAO.php
@@ -1,5 +1,6 @@
 <?php
 require_once  "basededonee.php";
+require_once  "BaseDeDonnee.php";
 class kayakDAO{
     public static function listeKayak()
     {
@@ -11,6 +12,16 @@ class kayakDAO{
 
     public static function ajouterKayak($informationKayak,$image)
     {
-        print_r($informationKayak);
+          
+        $MESSAGE_SQL_AJOUTER_JEU = "INSERT INTO kayak (titreAnnonce,descriptionAnnonce,type,adresse,cout,idMembre,image) VALUES (:titreAnnonce,:descriptionAnnonce,:type, :adresse,:cout,:idMembre,'$image')";
+        $requeteAjoutKayak = BaseDeDonnee::getConnexion() -> prepare($MESSAGE_SQL_AJOUTER_JEU); 
+
+        $requeteAjoutKayak->bindParam(':titreAnnonce', $informationKayak['titreAnnonce'], PDO::PARAM_STR);
+        $requeteAjoutKayak->bindParam(':descriptionAnnonce', $informationKayak['descriptionAnnonce'], PDO::PARAM_STR);
+        $requeteAjoutKayak->bindParam(':type', $informationKayak['type'], PDO::PARAM_STR);
+        $requeteAjoutKayak->bindParam(':adresse', $informationKayak['adresse'], PDO::PARAM_STR);
+        $requeteAjoutKayak->bindParam(':cout', $informationKayak['cout'], PDO::PARAM_STR);
+        $requeteAjoutKayak->bindParam(':idMembre', $informationKayak['idMembre'], PDO::PARAM_STR);
+        return $requeteAjoutKayak-> execute();
     }
 }
