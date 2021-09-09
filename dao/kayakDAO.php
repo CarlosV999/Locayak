8942e492569b9495069ce05032461b373f1eaf13
TMRomain <romainover51@gmail.com>
2021-09-09 20:50:40 +0000

    Transformation de Kayak DAO en class #15

--- dao/kayakDAO.php
+++ dao/kayakDAO.php
@@ -1,10 +1,16 @@
 <?php
 require_once  "basededonee.php";
+class kayakDAO{
+    public static function listeKayak()
+    {
+        $MESSAGE_SQL_RECUPERER = "SELECT * FROM `kayak`";
+        $requeteSelectKayak = getConnexion()->prepare($MESSAGE_SQL_RECUPERER);
+        $requeteSelectKayak->execute();
+        return $requeteSelectKayak->fetchAll();
+    }
 
-function listeKayak()
-{
-    $MESSAGE_SQL_RECUPERER = "SELECT * FROM `kayak`";
-    $requeteSelectKayak = getConnexion()->prepare($MESSAGE_SQL_RECUPERER);
-    $requeteSelectKayak->execute();
-    return $requeteSelectKayak->fetchAll();
+    public static function ajouterKayak($informationKayak,$image)
+    {
+        print_r($informationKayak);
+    }
 }
