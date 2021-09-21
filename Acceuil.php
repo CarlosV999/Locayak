00397cc963fa5d2bd8f6bbfdc15e980fc0b1f163
ericPellletier <pelletierric@gmail.com>
2021-09-21 18:03:51 +0000

    Passage du .html en .php

---
+++ Acceuil.php
@@ -0,0 +1,19 @@
+<?php
+  include_once "header.php";
+?>
+      <section>
+        <div class="zone-bienvenue">
+          <p class="message-bienvenue">
+            Bonjour et bienvenue chez Locayak, le site où vous pouvez
+            louer un kayak, ou mettre votre kayak en location !
+            
+          </p>
+        </div>
+      </section>
+
+      <?php
+       include_once "footer.php";
+       ?>     
+
+ </body>
+</html>
