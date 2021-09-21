ff2649989168f991178b02e45a4525add20e1197
CarlosV999 <carlos.descam@hotmail.fr>
2021-09-21 13:19:34 +0000

    maj header

---
+++ header.php
@@ -0,0 +1,32 @@
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Acceuil</title>
+  <link rel="stylesheet" href="css/menu.css?v=1">
+  <link rel="stylesheet" href="css/liste-kayak-admin.css?v=1">
+  <link rel="stylesheet" href="css/page-liste.css?v=1">
+  <link rel="stylesheet" href="css/stylesheet.css?v=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+</head>
+
+<body>
+    <div class="header">
+        <h1><a class="logo" href="Acceuil.php">LoCayak</a></h1>
+      </div>
+    <a  class="hamburger" href="javascript:void(0);" class="icon" onclick="ouvrirLeMenu()">
+      <i class="fa fa-bars"></i>
+    </a>
+
+
+    <ul id = "mesLiens">
+      <li><a href="Acceuil.php">Acceuil</a></li>
+      <li><a href="liste-kayaks.php">En Location</a></li>
+      <li><a href="#">Membre</a></li>
+      <li><a href="Projet.php">A propos</a></li>
+      <li><a href="#">Messagerie</a></li>
+    </ul> 
+  <script src="scripts/menu.js"></script>
+  
+</body>
