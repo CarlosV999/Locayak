75c00a15ef1f076e5c39a408e951fd920e4dba56
ericPellletier <pelletierric@gmail.com>
2021-09-23 19:48:30 +0000

    correction du footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer role="contentinfo" class="footer">
+            <section class="">
+            
+              <div><a href="Projet.php">À Propos de Locayak</a></div>
+              <div><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Suiver Nous sur Facebook</a></div>
+              <div><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Pour Nous Contacter</a></div>
+            </section>
+      </footer>
