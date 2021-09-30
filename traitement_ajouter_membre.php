8bafca6dbc1881427b9772709b4f9b4d9704ebfe
CarlosV999 <carlos.descam@hotmail.fr>
2021-09-30 21:52:27 +0000

    #13 traitement ajouter membre

--- traitement_ajouter_membre.php
+++ traitement_ajouter_membre.php
@@ -2,3 +2,14 @@
 require_once "configuration.php";
 
 require_once CHEMIN_DAO."MembreDAO.php";
+
+$filtresKayak = array();
+
+$filtresKayak['nom'] = FILTER_SANITIZE_STRING;
+$filtresKayak['prenom'] = FILTER_SANITIZE_STRING;
+$filtresKayak['adresse'] = FILTER_SANITIZE_STRING;
+$filtresKayak['email'] = FILTER_SANITIZE_STRING;
+$filtresKayak['description'] = FILTER_SANITIZE_STRING;
+$filtresKayak['cout'] = FILTER_SANITIZE_NUMBER_INT;
+$filtresKayak['illustration'] = FILTER_SANITIZE_NUMBER_INT;
+$filtresKayak['idMembre'] = "0";
