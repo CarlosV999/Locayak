85afe257c0fb8fda5a5b6e08b34c1550cf8afe66
CarlosV999 <carlos.descam@hotmail.fr>
2021-10-26 13:36:54 +0000

    confirmation paiement

---
+++ confirmation-Paiement.php
@@ -0,0 +1,27 @@
+<?php
+  include_once "header.php";
+?>
+
+<section>
+        <div class="Projet">
+          <p class="message-explication-projet">
+            Merci pour votre transaction.<br>
+            <br>
+            Vous allez être redirigé vers la page d'acceuil.<br>
+            <br>
+            si rien ne se passe cliquez sur le lien ci-dessous<br>
+            <br>
+            <a href="Acceuil.php">Cliquez-ici</a>
+          </p>
+        </div>
+      </section>
+
+
+
+
+<?php
+  include_once "footer.php";
+?>     
+
+ </body>
+</html>
