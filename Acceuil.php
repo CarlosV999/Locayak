cd7aa4469ec6c83f87aaade886b26480190b3426
CarlosV999 <carlos.descam@hotmail.fr>
2021-11-02 18:10:51 +0000

    Affichage dans l'acceuil des locations en cours, mit en commentaire pour faire autre chose

--- Acceuil.php
+++ Acceuil.php
@@ -1,17 +1,19 @@
 <?php
 require_once "configuration.php";
-  include_once "header.php";
-require_once "dao/LocationDAO.php";
-require_once "dao/KayakDAO.php";
-require_once "dao/MembreDAO.php";
-
+//require_once CHEMIN_DAO."LocationDAO.php";
+//require_once CHEMIN_DAO."KayakDAO.php";
+//require_once CHEMIN_DAO."MembreDAO.php";
+include_once "header.php";
 //Remplacer pour l'id recuperer de la session
+/*
 $email = $_SESSION['membreEmail'];
 $id = $_SESSION["idMembre"];
 
 $listeKayak = KayakDAO::listeKayakPourMembre($id);
 $membre = MembreDAO::recupererMembre($email);
+*/
 ?>
+
       <section>
         <div class="zone-bienvenue">
           <p class="message-bienvenue">
@@ -20,24 +22,38 @@ $membre = MembreDAO::recupererMembre($email);
           </p>
         </div>
       </section>
+
+<?php 
+  /*
+    if(isset($_SESSION['idMembre']))
+    {
+      
+?>
       <p class="nom">Mes Kayaks</p>
-    <div class="mes-kayak">
+      <div class="mes-kayak">
+      
+      <?php
+              foreach ($listeKayak as $kayak)
+               {
+                  //membreDAO::listeMembre();
+                  //$idMembre = $kayak["idMembre"];
+                  //print_r($listeMembre);
+              ?>
+      
+                  <div class="card-page-membre" style="width: 22rem;">
+                  <img class="card-img-top-membre" src="images/<?= $kayak['image'];?>" alt="Card image cap">
+                  <div class="card-body">
+                  <h5 class="card-title"><?= $kayak['titreAnnonce']; ?></h5>
+                  <p class="card-text"><?= $kayak["descriptionAnnonce"];?></p>
+                  <a href="Annonce.php?id=<?php echo $kayak['id']; ?>" class="btn btn-primary2-membre">Détail</a>
+                  </div>
+                  </div>
+              <?php
+                }
+      }
+      */
+  ?>
     
-    <?php
-            foreach ($listeKayak as $kayak) {
-                //membreDAO::listeMembre();
-                //$idMembre = $kayak["idMembre"];
-                //print_r($listeMembre);
-            ?>
-    
-    <div class="card-page-membre" style="width: 22rem;">
-        <img class="card-img-top-membre" src="images/<?= $kayak['image'];?>" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title"><?= $kayak['titreAnnonce']; ?></h5>
-            <p class="card-text"><?= $kayak["descriptionAnnonce"];?></p>
-            <a href="Annonce.php?id=<?php echo $kayak['id']; ?>" class="btn btn-primary2-membre">Détail</a>
-        </div>
-    </div>
 </br>
 </br>
 </br>
