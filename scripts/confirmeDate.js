2575d7fa1d0ed37ce590f93c1dd4df76a8750929
TMRomain <romainover51@gmail.com>
2021-11-04 15:21:24 +0000

    Changement dans le systeme de date #9 (4h)

--- scripts/confirmeDate.js
+++ scripts/confirmeDate.js
@@ -10,21 +10,7 @@ function configurerDate(dateNonFormater) {
   
 }
 $(document).ready(function() {
-const d = new Date();
-$("#dateFinDisponibiliter").datepicker({
-    minDate: d,
-    onSelect: function(dateText, inst) {
-    var date1 = new Date($("#dateDebutDisponibiliter").datepicker('getDate'));
-    var date2 = new Date($("#dateFinDisponibiliter").datepicker('getDate'));
-    date1Confirmation = document.getElementById("dateDebutDisponibiliter").value;
-    date2Confirmation = document.getElementById("dateFinDisponibiliter").value;
-    var difference = date1.getTime() - date2.getTime(); 
-    var days = Math.abs(Math.ceil(difference / (1000 * 3600 * 24)));
-    document.getElementById("textConfirmation").innerHTML = "Prix total: " + days*cout + "$";
-    // document.getElementById("amount").value =days*cout;
-    coutTotal =days*cout; 
-  }
-  });
+
 //Lors de la selection de la date
 $("#dateDebutDisponibiliter").datepicker({
   
