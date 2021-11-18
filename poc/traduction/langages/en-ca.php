10604b2cc0cf0df4d7af179feea9649b25767c43
CarlosV999 <carlos.descam@hotmail.fr>
2021-11-18 22:07:41 +0000

    #30 traduction accueil

--- poc/traduction/langages/en-ca.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $lang = array(
-        "message-bienvenue" => "Bonjour et bienvenue chez Locayak, le site où vous pouvez
-        louer un kayak, ou mettre votre kayak en location !"
-    );
-?>
