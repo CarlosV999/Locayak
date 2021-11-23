218031d8e0a39193c7d71544b8cc4476f466816b
CarlosV999 <carlos.descam@hotmail.fr>
2021-11-23 14:45:52 +0000

    #30 traductions (5 Heures)

--- membre/ajouter-kayak.php
+++ membre/ajouter-kayak.php
@@ -12,54 +12,54 @@ require_once "../administration/header-admin.php";
 <script src="../scripts/jquery-3.6.0.js"></script>
 <script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 <script type="text/javascript" src="../scripts/dates.js?<?php echo time(); ?>"></script>
-<title>Ajouter votre Kayak</title>
+<title><?= _('Ajouter votre Kayak')?></title>
 <body>
 <div class="container">
     <form action="traitement-ajouter-kayak.php" method="post" enctype="multipart/form-data">
-    <h3>Votre Kayak</h3>
-    <h4>Entre les information de votre Kayak</h4>
+    <h3><?= _('Votre Kayak')?></h3>
+    <h4><?= _('Entrez les information de votre Kayak')?></h4>
     <fieldset>
       <label for="titreAnnonce"></label>
-      <input placeholder="Titre de votre annonce" id="titreAnnonce" name="titreAnnonce" type="text" tabindex="1" required autofocus>
+      <input placeholder="<?= _('Titre de votre annonce')?>" id="titreAnnonce" name="titreAnnonce" type="text" tabindex="1" required autofocus>
     </fieldset>
     <fieldset>
       <label for="descriptionAnnonce"></label>
-      <textarea placeholder="Description de voter annonce" id="descriptionAnnonce" name="descriptionAnnonce" type="textarea" tabindex="1" required ></textarea>
+      <textarea placeholder="<?= _('Description de votre annonce')?>" id="descriptionAnnonce" name="descriptionAnnonce" type="textarea" tabindex="1" required ></textarea>
     </fieldset>
     <fieldset>
       <label for="adresse"></label>
-      <input placeholder="Votre adresse" id="adresse" name="adresse" type="text" tabindex="1" required autofocus>
+      <input placeholder="<?= _('Votre adresse')?>" id="adresse" name="adresse" type="text" tabindex="1" required autofocus>
     </fieldset>
     <fieldset>
       <label for="idMembre"></label>
       <input id="idMembre" name="idMembre" type="hidden" value=<?= $_SESSION['idMembre'] ?> tabindex="1" required autofocus>
     </fieldset>
     <fieldset>
-      <label for="type">Taille du Kayak</label></br>
-      <input type="radio" id="type" name="type" value="Petit"> Petit<br>
-      <input type="radio" id="type" name="type" value="Moyen"> Moyen<br>
-      <input type="radio" id="type" name="type" value="Grand"> Grand
+      <label for="type"><?= _('Taille du Kayak')?></label></br>
+      <input type="radio" id="type" name="type" value="Petit"><?= _(' Petit')?><br>
+      <input type="radio" id="type" name="type" value="Moyen"><?= _(' Moyen')?><br>
+      <input type="radio" id="type" name="type" value="Grand"><?= _(' Grand')?>
     </fieldset>
     <fieldset>
       <label for="cout"></label>
-      <input placeholder="Coût de la location" id="cout" name="cout" type="number" min="0" step="any" tabindex="3" required>
+      <input placeholder="<?= _('Coût de la location')?>" id="cout" name="cout" type="number" min="0" step="any" tabindex="3" required>
     </fieldset>
     <fieldset>
-      <label for="illustration">Illustration</label></br>
+      <label for="illustration"><?= _('Illustration')?></label></br>
       <input type="file" id="illustration" name="illustration" required>
     </fieldset>
     
     </br>
     <fieldset>
-      <p>Debut des disponibilité: <input type = "text" onblur="validerDate();" name="dateDebutDisponibiliter" id = "dateDebutDisponibiliter"></p>
-      <p>Fin des disponibilité: <input type = "text" onblur="validerDate();" name="dateFinDisponibiliter" id = "dateFinDisponibiliter"></p>
+      <p><?= _('Début des disponibilités: ')?><input type = "text" onblur="validerDate();" name="dateDebutDisponibiliter" id = "dateDebutDisponibiliter"></p>
+      <p><?= _('Fin des disponibilités: ')?><input type = "text" onblur="validerDate();" name="dateFinDisponibiliter" id = "dateFinDisponibiliter"></p>
       </fieldset>
       </br>
     <fieldset>
         <?php
              if(isset($_SESSION["idMembre"]))
              {
-                ?><button type="submit" id="envoyer-ajout">Enregistrer le Kayak</button><?php
+                ?><button type="submit" id="envoyer-ajout"><?= _('Enregistrer le Kayak')?></button><?php
              }
         ?>
       
